c89d20299bfa92ed10d3b4b35c1b1798eed00add
markxbrooks <mark.x.brooks@gmail.com>
2023-04-21 15:43:21 -0400

    Chris won (maybe ChatGPT)

--- mr_autophaser.py
+++ mr_autophaser.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+from pathlib import Path
 from phaser import *
 import argparse
-from Bio.PDB import PDBParser
+from Bio.PDB import PDBParser, PPBuilder
+from Bio import SeqIO
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--mtz", metavar="mtz", help="MTZ file of interest", type=str)
@@ -9,9 +12,33 @@ parser.add_argument("-p", "--pdb1", metavar="pdb1", help="PDB file of interest 1
 parser.add_argument("-q", "--pdb2", metavar="pdb2", help="PDB file of interest 2", type=str)
 args = parser.parse_args()
 
-#p = PDBParser()
-#pdb1 = p.get_structure("pdb1", args.pdb1)
-#pdb2 = p.get_structure("pdb2", pdb2.pdb2)
+p1_stem = Path(args.pdb1).stem
+p2_stem = Path(args.pdb2).stem
+
+p = PDBParser(QUIET=True)
+pdb1_structure = p.get_structure("pdb1", args.pdb1)
+ppb1 = PPBuilder()
+for pp1 in ppb1.build_peptides(pdb1_structure):
+    record1_str = pp1.get_sequence().__str__()
+    
+pdb2_structure = p.get_structure("pdb2", args.pdb2)
+ppb2 = PPBuilder()
+for pp2 in ppb2.build_peptides(pdb2_structure):
+    record2_str = pp2.get_sequence().__str__()
+    
+print(f">{p1_stem}")
+print(record1_str)
+print(f">{p2_stem}")
+print(record2_str)
+
+p1_analysis = ProteinAnalysis(str(record1_str))
+p1_mr = p1_analysis.molecular_weight()
+
+p2_analysis = ProteinAnalysis(str(record2_str))
+p2_mr = p2_analysis.molecular_weight()
+
+print(f"{p1_stem} pdb1 mr: {p1_mr:.1f} Da")
+print(f"{p2_stem} pdb2 mr: {p2_mr:.1f} Da")
 
 i = InputMR_DAT()
 i.setHKLI(args.mtz)
@@ -27,8 +54,8 @@ if r.Success():
     i.setROOT("beta_blip_auto")
     i.addENSE_PDB_ID("pdb1",args.pdb1,1.0)
     i.addENSE_PDB_ID("pdb2",args.pdb2,1.0)
-    i.addCOMP_PROT_MW_NUM(28853,1)
-    i.addCOMP_PROT_MW_NUM(17522,1)
+    i.addCOMP_PROT_MW_NUM(p1_mr,1)
+    i.addCOMP_PROT_MW_NUM(p2_mr,1)
     i.addSEAR_ENSE_NUM("pdb1",1)
     i.addSEAR_ENSE_NUM("pdb2",1)
     i.setMUTE(True)
